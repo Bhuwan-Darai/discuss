cace50488c45418fff020dff65220f59096d6072
bhuwan-darai <dr.darai29@gmail.com>
2024-03-27 22:50:15 +0545

    recieving form data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import TopicCreateForm from "@/components/topics/topics-create-form";
 
-export default async function Home() {
+export default function Home() {
   return (
-    <div className="grid grid-col-4">
+    <div className="grid grid-cols-4 gap-4 p-4">
       <div className="col-span-3">
         <h1 className="text-xl m-2">Top Post</h1>
       </div>
